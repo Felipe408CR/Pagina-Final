d62da39f1ad135c54be309842c60040a9c13e269
Your Name <you@example.com>
2022-09-30 12:04:39 -0500

    formularios

--- src/app/formularioregistro/formularioregistro.component.ts
+++ src/app/formularioregistro/formularioregistro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup, FormBuilder, Validators} from '@angular/forms'
 
 @Component({
   selector: 'app-formularioregistro',
@@ -7,9 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormularioregistroComponent implements OnInit {
 
-  constructor() { }
+  formulario!:FormGroup;
+
+  constructor(public fabricaDiccionario:FormBuilder) { }
 
   ngOnInit(): void {
+
+    this.formulario=this.inicializarFormulario()
+
+  }
+
+  public analizarFormulario():void{
+    console.log(this.formulario.value)
+  }
+
+  public inicializarFormulario():FormGroup{
+    return this.fabricaDiccionario.group({
+      iup:['Juan',[Validators.required,Validators.minLength(6)]],
+      tiporemitente:['empresa',[Validators.required]],
+      idremitente:['123456789',[Validators.required]],
+      nombreremitente:['ANDERCOL',[Validators.required]],
+      deptoremitente:['Antioquia',[Validators.required]],
+      municipioremitente:['Envigado',[Validators.required]],
+      direccionremitente:['cr 50 sur #10-20',[Validators.required]],
+      tipodestinatario:['empresa',[Validators.required]],
+      iddestinatario:['1112131415',[Validators.required]],
+      nombredestinatario:['COMFAMA',[Validators.required]],
+      deptodestinatario:['Antioquia',[Validators.required]],
+      municipiodestinatario:['Medellin',[Validators.required]],
+      direcciondestinatario:['cll 50#32-20',[Validators.required]],
+    })
   }
 
 }
