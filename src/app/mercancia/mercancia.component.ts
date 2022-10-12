74c6ebf603c959c2f504d3d4c8b24406538bd46f
308 <308@academicos.cesde.edu.co>
2022-10-12 12:05:16 -0500

    Se crea pagina de mercancias y estilos

--- src/app/mercancia/mercancia.component.ts
+++ src/app/mercancia/mercancia.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { id } from '@swimlane/ngx-charts';
+import { MercanciasService } from '../services/mercancias.service';
+import { ZonasService } from '../services/zonas.service';
 
 @Component({
   selector: 'app-mercancia',
   templateUrl: './mercancia.component.html',
   styleUrls: ['./mercancia.component.css']
 })
-export class MercanciaComponent implements OnInit {
+export class MercanciaComponent  {
 
-  constructor() { }
+  datosZonas:any[]=[];
 
-  ngOnInit(): void {
+  constructor(
+    public fabricaDiccionario:FormBuilder,
+    public servicioZona:ZonasService,
+    public servicioMercancia:MercanciasService
+    ) {
+
+    this.servicioZona.consultarZonas()
+      .subscribe(respuesta=>{
+        console.log(respuesta);
+        this.datosZonas=respuesta
+      })
+
   }
 
+  retirarMercancia(idMercancia:any):void{
+
+    this.servicioMercancia.retirarMercancia(idMercancia)
+    .subscribe(respuesta=>{
+      console.log(respuesta)
+      window.location.reload()
+    })
+    
+  }
+
+  public buscarMercanciaId():FormGroup{
+    return this.fabricaDiccionario.group({
+      iup:['',Validators.required]
+    })
+  }
+ 
 }
